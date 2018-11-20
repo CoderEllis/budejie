e970f50828a01b45306ccfdd0b3eeb7a10dad57c
ellis <156228219@qq.com>
2018-11-20 22:24:29 +0800

    初始化项目

---
+++ budejie/budejie/Classes/Main/Contorller/ELTabBarController.h
@@ -0,0 +1,17 @@
+//
+//  ELTabBarController.h
+//  budejie
+//
+//  Created by Soul Ai on 20/11/2018.
+//  Copyright © 2018 Soul Ai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ELTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
